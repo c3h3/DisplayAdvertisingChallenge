510bd5dcdebcc17ec314f15bc90bfd5171d55ee2
Chia-Chi Chang <c3h3.tw@gmail.com>
2014-09-07 21:59:12 +0800

    update new divide_all_data_columns.py

--- bin/divide_all_data_columns.py
+++ bin/divide_all_data_columns.py
@@ -1,13 +1,24 @@
 
 from multiprocessing import Pool   
 
-dividing_jobs = [xx for xx in COLUMN_NAMES if xx not in list_all_data_columns()]
+# dividing_jobs = [xx for xx in COLUMN_NAMES if xx not in list_all_data_columns()]
+# 
+# print "dividing_jobs = ", dividing_jobs
+# 
+# p = Pool(processes = MAX_N_DATA_COLUMN_DIVIDERS)
+# p.map(pickle_one_data_column, dividing_jobs)
+# 
+# p.close()
+# p.join()
 
-print "dividing_jobs = ", dividing_jobs
+from hunkaggle.criteo.settings import TRAINING_COLUMN_NAMES
+from hunkaggle.criteo.settings import MAX_N_DATA_COLUMN_DIVIDERS
+from hunkaggle.criteo import tools
 
 p = Pool(processes = MAX_N_DATA_COLUMN_DIVIDERS)
-p.map(pickle_one_data_column, dividing_jobs)
-
+p.map(tools.convert_column_to_blz, TRAINING_COLUMN_NAMES)
+ 
 p.close()
 p.join()
+
 
--- hunkaggle/criteo/tools.py
+++ hunkaggle/criteo/tools.py
@@ -29,54 +29,58 @@ def get_one_column_data_list(colname, data_csv_path=TRAINING_DATA_PATH):
 
 
 def convert_column_to_blz(colname):
-    if colname.startswith("C"):
-        train_data = get_one_column_data_list(colname, data_csv_path=TRAINING_DATA_PATH)
-        test_data = get_one_column_data_list(colname, data_csv_path=TESTING_DATA_PATH)
-        
-        train_data_has_missing_values = "" in train_data
-        test_data_has_missing_values = "" in test_data
-        
-        
-        all_data_np_array = np.array(train_data + test_data)
-        
-        uu, ui, ii = np.unique(all_data_np_array,return_index=True,return_inverse=True)
-        
-        train_blz_root = os.path.join(TRAINING_BLZ_PATH,colname)
-        test_blz_root = os.path.join(TESTING_BLZ_PATH,colname)
-        
-        train_barr = blz.barray(ii[:len(train_data)],rootdir=train_blz_root)
-        train_barr.attrs["states"] = uu.tolist()
-        train_barr.attrs["has_missings"] = train_data_has_missing_values
-        
-        test_barr = blz.barray(ii[len(train_data):],rootdir=test_blz_root)
-        test_barr.attrs["states"] = uu.tolist()
-        test_barr.attrs["has_missings"] = test_data_has_missing_values
-        
-    else:
-        data = get_one_column_data_list(colname, data_csv_path=TRAINING_DATA_PATH)
-        has_missing = "" in data
-        missing_value = int(1.0e16)
-        if has_missing:
-            int_data = map(lambda xx: missing_value if xx in [""] else int(xx), data)
-        else:
-            int_data = map(int, data)
+    
+    if not colname in os.listdir(TRAINING_BLZ_PATH):
+    
+        if colname.startswith("C"):
             
-        blz_root = os.path.join(TRAINING_BLZ_PATH,colname)
-        barr = blz.barray(int_data,rootdir=blz_root)
-        barr.attrs["missing_value"] = missing_value
-        barr.attrs["has_missing"] = has_missing    
-        
-        if colname != "Label":
-            data = get_one_column_data_list(colname, data_csv_path=TESTING_DATA_PATH)
+            train_data = get_one_column_data_list(colname, data_csv_path=TRAINING_DATA_PATH)
+            test_data = get_one_column_data_list(colname, data_csv_path=TESTING_DATA_PATH)
+            
+            train_data_has_missing_values = "" in train_data
+            test_data_has_missing_values = "" in test_data
+            
+            
+            all_data_np_array = np.array(train_data + test_data)
+            
+            uu, ui, ii = np.unique(all_data_np_array,return_index=True,return_inverse=True)
+            
+            train_blz_root = os.path.join(TRAINING_BLZ_PATH,colname)
+            test_blz_root = os.path.join(TESTING_BLZ_PATH,colname)
+            
+            train_barr = blz.barray(ii[:len(train_data)],rootdir=train_blz_root)
+            train_barr.attrs["states"] = uu.tolist()
+            train_barr.attrs["has_missings"] = train_data_has_missing_values
+            
+            test_barr = blz.barray(ii[len(train_data):],rootdir=test_blz_root)
+            test_barr.attrs["states"] = uu.tolist()
+            test_barr.attrs["has_missings"] = test_data_has_missing_values
+            
+        else:
+            data = get_one_column_data_list(colname, data_csv_path=TRAINING_DATA_PATH)
             has_missing = "" in data
             missing_value = int(1.0e16)
             if has_missing:
                 int_data = map(lambda xx: missing_value if xx in [""] else int(xx), data)
             else:
                 int_data = map(int, data)
-            
-            blz_root = os.path.join(TESTING_BLZ_PATH,colname)
+                
+            blz_root = os.path.join(TRAINING_BLZ_PATH,colname)
             barr = blz.barray(int_data,rootdir=blz_root)
             barr.attrs["missing_value"] = missing_value
-            barr.attrs["has_missing"] = has_missing
+            barr.attrs["has_missing"] = has_missing    
+            
+            if colname != "Label":
+                data = get_one_column_data_list(colname, data_csv_path=TESTING_DATA_PATH)
+                has_missing = "" in data
+                missing_value = int(1.0e16)
+                if has_missing:
+                    int_data = map(lambda xx: missing_value if xx in [""] else int(xx), data)
+                else:
+                    int_data = map(int, data)
+                
+                blz_root = os.path.join(TESTING_BLZ_PATH,colname)
+                barr = blz.barray(int_data,rootdir=blz_root)
+                barr.attrs["missing_value"] = missing_value
+                barr.attrs["has_missing"] = has_missing
         
