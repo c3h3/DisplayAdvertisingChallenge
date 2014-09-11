5e5ec9169c17bfe7ef3a4ab168fd36ec26170b78
Qmal <qmal@localhost.localdomain>
2014-09-11 00:29:16 -0400

    celery module

---
+++ bin/predict_celery.py
@@ -0,0 +1,25 @@
+from hunkaggle.criteo import tools
+from hunkaggle.criteo.settings import SOURCE_DATA_DIR
+from hunkaggle.criteo.settings import TRAINING_COLUMN_NAMES
+from hunkaggle.criteo.settings import TESTING_COLUMN_NAMES
+import blz, os
+import numpy as np
+
+from predict_celery.tasks import app
+from predict_celery.tasks import modelPredictor
+from settings import *
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
+model_path = SOURCE_DATA_DIR + "models.pickle"
+with open(model_path, "rb") as rf:
+    models = pickle.load(rf)
+
+num_models = len(models)
+print num_models
+
+jobs = [(model_path, ind, tools.TESTING_BLZ_PATH, TESTING_COLUMN_NAMES) for ind in range(num_models)]
+
+results = [modelPredictor.delay(job) for job in jobs]
