ae690e2db786f525ae623769f44fdf962b3f44cb
Chia-Chi Chang <c3h3.tw@gmail.com>
2014-09-16 09:05:19 +0800

    change parameters

--- bin/brute_force_training_random_forest.py
+++ bin/brute_force_training_random_forest.py
@@ -1,7 +1,7 @@
 from hunkaggle.criteo.models import *
 
 total_n = blz.open(os.path.join(tools.TRAINING_BLZ_PATH,TRAINING_COLUMN_NAMES[0])).shape[0]
-m_groups = 40
+m_groups = 20
 dived_n = total_n / m_groups if total_n % m_groups == 0 else total_n / m_groups + 1
 training_data_slices = map(lambda xx:slice(*xx),tools.get_separation_pairs(total_n,dived_n))
 
@@ -11,8 +11,8 @@ training_data_slices = map(lambda xx:slice(*xx),tools.get_separation_pairs(total
  
 def _create_new_model_with_origin_training_data(one_slice):
     from sklearn.ensemble import RandomForestClassifier
-    rf_params = {'min_samples_split': 10, 'n_estimators': 200, "n_jobs":12}
-    model_params = {"model_series":"RFmss20ne130-40Groups", 
+    rf_params = {'min_samples_split': 20, 'n_estimators': 200, "n_jobs":12}
+    model_params = {"model_series":"RFmss20ne200-20Groups", 
                     "model_type": RandomForestClassifier, 
                     "model_parameters":rf_params, 
                     "data_slice":one_slice,
