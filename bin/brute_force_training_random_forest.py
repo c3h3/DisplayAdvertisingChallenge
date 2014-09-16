a976b0bf4f691d88c3bdc9756936a7b1da48dc44
Chia-Chi Chang <c3h3.tw@gmail.com>
2014-09-16 08:58:39 +0800

    update running script

--- bin/brute_force_training_random_forest.py
+++ bin/brute_force_training_random_forest.py
@@ -6,22 +6,25 @@ dived_n = total_n / m_groups if total_n % m_groups == 0 else total_n / m_groups 
 training_data_slices = map(lambda xx:slice(*xx),tools.get_separation_pairs(total_n,dived_n))
 
 # from hunkaggle.criteo.settings import MAX_N_DATA_COLUMN_DIVIDERS
-from multiprocessing import Pool 
- 
-MAX_N_DATA_COLUMN_DIVIDERS = 4
+# from multiprocessing import Pool 
+# MAX_N_DATA_COLUMN_DIVIDERS = 4
  
 def _create_new_model_with_origin_training_data(one_slice):
     from sklearn.ensemble import RandomForestClassifier
-    rf_params = {'min_samples_split': 20, 'n_estimators': 130, "n_jobs":3}
+    rf_params = {'min_samples_split': 10, 'n_estimators': 200, "n_jobs":12}
     model_params = {"model_series":"RFmss20ne130-40Groups", 
                     "model_type": RandomForestClassifier, 
                     "model_parameters":rf_params, 
                     "data_slice":one_slice,
-                    "predict_limit_instances":1000000}
+                    "predict_limit_instances":5000000}
     create_new_model_with_origin_training_data(**model_params)
  
-p = Pool(processes = MAX_N_DATA_COLUMN_DIVIDERS)
-p.map(_create_new_model_with_origin_training_data, training_data_slices)
-  
-p.close()
-p.join()
+ 
+for one_slice in training_data_slices:
+    _create_new_model_with_origin_training_data(one_slice)
+
+# p = Pool(processes = MAX_N_DATA_COLUMN_DIVIDERS)
+# p.map(_create_new_model_with_origin_training_data, training_data_slices)
+#   
+# p.close()
+# p.join()
